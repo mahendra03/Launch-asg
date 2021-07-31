26f2a4709fbeb9803fbd2617be3cfb049f696166
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-31 17:36:16 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -11,6 +11,11 @@ resource "aws_autoscaling_group" "web" {
   desired_capacity     = 1
   max_size             = 1
   
+  health_check_type    = "ELB"
+  load_balancers = [
+    aws_elb.web_elb.id
+  ]
+  
   launch_configuration = aws_launch_configuration.as_conf.name
 
     vpc_zone_identifier  = [
