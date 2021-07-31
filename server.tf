9ff9ff1958468267900b3c50018219d38798c5a1
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-31 17:56:13 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -38,6 +38,7 @@ resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_up" {
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods = "2"
   metric_name = "CPUUtilization"
+  namespace = "AWS/EC2"
   period = "120"
   statistic = "Average"
   threshold = "60"
