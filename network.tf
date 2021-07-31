9c33a8ed75a62eabf0fac2f0b115043df45682ae
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-31 17:08:54 +0530

    Create network.tf

---
+++ network.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "main" {
+   cidr_block="10.0.0.0/16"
+ }
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+  
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+  
+}
