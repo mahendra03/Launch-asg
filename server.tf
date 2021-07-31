32fc7e8c219876ce6fd71a38e0ded18ddc3f7c18
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-31 17:15:14 +0530

    Create server.tf

---
+++ server.tf
@@ -0,0 +1,21 @@
+resource "aws_launch_configuration" "as_conf" {
+  name          = "web_config1"
+  image_id      = "ami-04db49c0fb2215364"
+  instance_type = "t2.micro"
+}
+
+resource "aws_autoscaling_group" "web" {
+  name = "${aws_launch_configuration.as_conf.name}-asg"
+
+  min_size             = 1
+  desired_capacity     = 1
+  max_size             = 1
+  
+  launch_configuration = aws_launch_configuration.as_conf.name
+
+    vpc_zone_identifier  = [
+    aws_subnet.main.id,
+    aws_subnet.main1.id
+  ]
+
+}
