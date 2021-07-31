784d62b0f066097fe0cf80dcba712d715d8ce4a1
mahendra03 <49598426+mahendra03@users.noreply.github.com>
2021-07-31 17:12:35 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -3,14 +3,14 @@ resource "aws_vpc" "main" {
  }
 
 resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.my_vpc.id
+  vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
   
 }
 
 resource "aws_subnet" "main1" {
-  vpc_id     = aws_vpc.my_vpc.id
+  vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "ap-south-1b"
   
